41c3b91a5cb3f164087a35a0e0eb735b1f4ac5b5
Myze <myze2016@gmail.com>
2019-05-10 11:51:52 +0800

    Insurance Monitoring System

---
+++ assets/js/test.js
@@ -0,0 +1,29 @@
+
+   $(document).ready(function(){
+     $('#userform').submit(function(e){
+      e.preventDefault();
+      var url  = $(this).attr('action');
+
+      $.ajax({
+        url : url,
+        method: 'POST',
+        data : $(this).serialize(),
+        success: function(res){
+           $('#success-message').text(res.message);
+        },
+        error: function(err){
+          var $errors  =  JSON.parse(err.responseText);
+
+          alert($errors.username); 
+          alert($errors.email);
+          alert($errors.password); 
+         
+          
+        }
+
+    })
+      
+  })
+
+})
+
